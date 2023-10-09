a8929da6e95aecdecec7c193e69dd4b097c238a7
sudhanshu7sb <sudhanshu7.sb@gmail.com>
2023-10-09 21:57:25 +0530

    Add header

---
+++ src/app/components/FeaturesDropDown.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeaturesDropDown = () => {
+  return (
+    <div className="absolute -bottom-8  w-[50%] h-[50%] bg-white text-black">FeaturesDropDown</div>
+  )
+}
+
+export default FeaturesDropDown
